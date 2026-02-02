5473d918aa3ea2a31ca40dfe78f9089a2a76422a
Aatoz <aatozb@gmail.com>
2026-02-02 00:48:07 -0500

    Configs for linting, prettier, tsconfig, and vitest, tsconfig

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  ignorePatterns: ['dist/', 'node_modules/', 'coverage/'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/strict-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/explicit-function-return-type': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/prefer-readonly': 'error',
+  },
+};
